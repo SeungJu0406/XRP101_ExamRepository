fc4d273557286dc7f44a8ce3c0f1edea964a15ec
SeungJu0406 <nss8164@gmail.com>
2024-11-19 10:59:59 +0900

    Fix : 문제 2번 버그 픽스

--- Q_02/Assets/Scripts/PlayerMovement.cs
+++ Q_02/Assets/Scripts/PlayerMovement.cs
@@ -28,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
         direction.z = Input.GetAxisRaw("Vertical");
 
         if (direction == Vector3.zero) return;
+        direction.Normalize(); // 벡터값 일반화 추가
         
         transform.Translate(_status.MoveSpeed * Time.deltaTime * direction);
     }
